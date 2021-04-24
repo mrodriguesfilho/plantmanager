cab4d8959a92b05d06054ae2d7a3d7f34abfeccf
Márcio Rodrigues Filho <rodriguesfilhomarcio@gmail.com>
2021-04-24 15:37:57 -0300

    Dia 3 done

---
+++ src/components/Header.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { StyleSheet, Text, Image, View } from 'react-native';
+import colors from '../styles/colors';
+import userImg from '../assets/github.png';
+import fonts from '../styles/fonts';
+
+export function Header() {
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text style={styles.greeting}>Olá,</Text>
+                <Text style={styles.userName}>marciow</Text>
+            </View>
+            <Image source={userImg} style={styles.image} />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        height: 160,
+    },
+
+    image: {
+        width: 70,
+        height: 70,
+        borderRadius: 40,
+    },
+
+    greeting: {
+        fontSize: 32,
+        color: colors.heading,
+        fontFamily: fonts.text,
+    },
+
+    userName: {
+        fontSize: 32,
+        fontFamily: fonts.heading,
+        color: colors.heading,
+        lineHeight: 40,
+    },
+});
