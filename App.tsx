6b20030af9c9c0ba6591fdf965ba8ca1de36444b
Márcio Rodrigues Filho <rodriguesfilhomarcio@gmail.com>
2021-05-06 14:25:43 -0300

    Adicionei a api de notificacoes - App finalizado

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import AppLoading from 'expo-app-loading';
+
 import {
     useFonts,
     // eslint-disable-next-line camelcase
--- src/pages/MyPlants.tsx
+++ src/pages/MyPlants.tsx
@@ -5,11 +5,10 @@ import { formatDistance } from 'date-fns';
 /*eslint-disable-line*/
 // eslint-disable-next-line import/no-duplicates
 import { pt } from 'date-fns/locale';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Header } from '../components/Header';
 import colors from '../styles/colors';
 import waterdrop from '../assets/waterdrop.png';
-import { loadPlant, PlantProps, StoragePlantProps } from '../libs/storage';
+import { loadPlant, PlantProps, removePlant } from '../libs/storage';
 import fonts from '../styles/fonts';
 import { PlantCardSecondary } from '../components/PlantCardSecondary';
 import { Load } from '../components/Load';
@@ -30,20 +29,7 @@ export function MyPlants() {
                 text: 'Sim',
                 onPress: async () => {
                     try {
-                        const data = await AsyncStorage.getItem(
-                            '@plantmanager:plants',
-                        );
-                        const plants = data
-                            ? (JSON.parse(data) as StoragePlantProps)
-                            : {};
-
-                        delete plants[plant.id];
-
-                        await AsyncStorage.setItem(
-                            '@plantmanager:plants',
-                            JSON.stringify(plants),
-                        );
-
+                        await removePlant(plant.id);
                         setMyPlants((oldData) =>
                             oldData.filter((item) => item.id !== plant.id),
                         );
